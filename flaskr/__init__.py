e0655e01a48eb9edc23832bf008cbf7244bf1597
Ian-Hennessy <ihennessy3@gatech.edu>
2026-02-17 22:01:32 -0500

    initial commit

---
+++ flaskr/__init__.py
@@ -0,0 +1,29 @@
+import os 
+
+from flask import Flask
+
+from db import init_app
+def create_app(test_config=None):
+    # create and configure app 
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config_py', silent=True)
+
+    else: 
+        app.config.from_mapping(test_config)
+
+        # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
+    init_app(app)
+    return app
---
+++ flaskr/db.py
@@ -0,0 +1,47 @@
+import sqlite3
+from datetime import datetime 
+
+import click 
+
+from flask import current_app, g 
+
+def get_db():
+    if 'db' not in g: 
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+
+def close_db():
+    db = g.pop('db',None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+@click.command('init-db')
+def init_db_command():
+    # Clear existing data and initialize a new database 
+    init_db()
+    click.echo("Database initialized!")
+
+sqlite3.register_converter(
+    "timestamp", lambda v: datetime.fromisoformat(v.decode())
+)
+
+def init_app(app):
+    # Tells Flask to run the close_db command
+    # after returning response from server
+    app.teardown_appcontest(close_db)
+
+    # reveals the init-db command to the app 
+    # it can be run by the client 
+    app.cli.add_command(init_db_command)
+
+    
